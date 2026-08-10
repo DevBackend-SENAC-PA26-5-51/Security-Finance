0bf349c78698190806c7e53e2f2f58d14eb50bd8
Raphael <raphael.massaro08@gmail.com>
2026-08-10 18:24:26 +0000

    criei a pasta autenticação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AutenticacaoModule } from './autenticacao/autenticacao.module';
 
 @Module({
-  imports: [],
+  imports: [AutenticacaoModule],
   controllers: [AppController],
   providers: [AppService],
 })
