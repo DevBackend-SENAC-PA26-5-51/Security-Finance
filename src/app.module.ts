8bdaa6eb39fecc1b3e088dc5550531bcb6c519dc
DeaneAraujo <deanejessicaaraujo@gmail.com>
2026-08-11 19:57:39 +0000

    foi criado um relatorio com total do faturamento em um ano

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
+import { FaturamentoController } from './faturamento/faturamento.controller.js';
+import { FaturamentoService } from './faturamento/faturamento.service.js';
+import { FaturamentoModule } from './faturamento/faturamento.module.js';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, FaturamentoModule],
+  controllers: [AppController, FaturamentoController],
+  providers: [AppService, FaturamentoService],
 })
 export class AppModule {}
