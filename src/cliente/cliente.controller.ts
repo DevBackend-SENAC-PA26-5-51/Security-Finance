15fbb06c9aef258d95371220e3c569242e6f1bf3
Raphael <raphael.massaro08@gmail.com>
2026-08-11 18:29:51 +0000

    Atualização da task-002 com a implementação de proteger rotas privadas e as rotas agora exigem autenticação

--- src/cliente/cliente.controller.ts
+++ src/cliente/cliente.controller.ts
@@ -2,9 +2,11 @@ import {
   Controller,
   Get,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 
 import { ClienteService } from './cliente.service.js';
+import { JwtAuthGuard } from '../autenticacao/guards/jwt-auth.guard.js';
 
 @Controller('clientes')
 export class ClienteController {
@@ -15,5 +17,13 @@ export class ClienteController {
   @Get('buscar')
   async buscar(@Query('email') email: string) {
     return this.clienteService.buscarPorEmail(email);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('privada')
+  async rotaPrivada() {
+    return {
+      mensagem: 'Você está autenticado!',
+    };
   }
 }
