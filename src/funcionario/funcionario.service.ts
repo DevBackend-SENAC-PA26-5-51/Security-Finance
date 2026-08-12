83baa13638b3153ca392c9590a6365db97c666c1
taina lisboa <tainna.lisboa54@gmail.com>
2026-08-12 20:32:49 +0000

    Adicionado as validações de entrada para o funcionario.

--- src/funcionario/funcionario.service.ts
+++ src/funcionario/funcionario.service.ts
@@ -1,11 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service.js';
+import { CreateFuncionarioDto } from './dto/create-funcionario.dto.js';
+import * as bcrypt from 'bcrypt'; 
 
 @Injectable()
 export class FuncionarioService {
   constructor(
     private readonly prisma: PrismaService,
   ) {}
+
+  
+  async findAll() {
+    return this.prisma.funcionario.findMany();
+  }
+
+  async create(createFuncionarioDto: CreateFuncionarioDto) {
+    
+    const emailExistente = await this.buscarPorEmail(createFuncionarioDto.email);
+    if (emailExistente) {
+      throw new BadRequestException('Já existe um funcionário cadastrado com este e-mail.');
+    }
+
+    
+    const hashedPassword = await bcrypt.hash(createFuncionarioDto.senha, 10);
+   
+    this.prisma.funcionario.create({
+      data: {
+        nome: createFuncionarioDto.nome,
+        CPF: createFuncionarioDto.CPF,
+        data_nascimento: new Date(createFuncionarioDto.data_nascimento),
+        cargo: createFuncionarioDto.cargo,
+        email: createFuncionarioDto.email,
+        senha: hashedPassword, 
+        data_admissao: new Date(createFuncionarioDto.data_admissao),
+        telefone: createFuncionarioDto.telefone,
+        Enderecos_Clientes_ID: Number(createFuncionarioDto.Enderecos_Clientes_ID),
+        ativo: 1
+      },
+    });
+
+    return {
+      Mensagem: "Funcionário registrado com sucesso!"
+    }
+  }
 
   async buscarPorEmail(email: string) {
     return this.prisma.funcionario.findFirst({
